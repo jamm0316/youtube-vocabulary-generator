835119be6e5bfe79c8656d998ca45bbc401f6266
jamm0316 <jamm0316@naver.com>
2025-11-03 16:02:59 +0900

    test: PlanTest 클래스 추가
    1. PlanTest 클래스 추가하여 Plan 도메인 테스트 코드 작성한다.
    2. Plan 생성 테스트와 PlanType 변경 테스트 추가한다.
    3. 동일 PlanType 변경 시 예외 처리 검증 테스트 포함한다.
    
    Resolves #2

---
+++ src/test/java/com/posicube/assignment/domain/plan/PlanTest.java
@@ -0,0 +1,56 @@
+package com.posicube.assignment.domain.plan;
+
+import com.posicube.assignment.common.exception.BaseException;
+import com.posicube.assignment.plan.domain.entity.Plan;
+import com.posicube.assignment.plan.domain.entity.PlanType;
+import com.posicube.assignment.plan.exception.PlanExceptionStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PlanTest {
+
+    @Test
+    @DisplayName("생성 성공:PlanType.LITE 정상 생성")
+    public void create_plan_success() throws Exception {
+        //given
+        Plan lite = Plan.createLite();
+
+        //then
+        assertThat(lite.getType()).isEqualTo(PlanType.LITE);
+        assertThat(lite.getTokens().quota()).isEqualTo(PlanType.LITE.getQuota());
+        assertThat(lite.getTokens().remainingTokens()).isEqualTo(PlanType.LITE.getQuota());
+    }
+
+    @Test
+    @DisplayName("changePlanType 메서드 성공: PlanType 변경 성공")
+    public void change_plan_type_success() throws Exception {
+        //given
+        Plan plan = Plan.createLite();
+
+        //when&then
+        plan.changePlanType(PlanType.PRO);
+        assertThat(plan.getType()).isEqualTo(PlanType.PRO);
+        assertThat(plan.getTokens().quota()).isEqualTo(PlanType.PRO.getQuota());
+        assertThat(plan.getTokens().remainingTokens()).isEqualTo(PlanType.PRO.getQuota());
+
+        plan.changePlanType(PlanType.LITE);
+        assertThat(plan.getType()).isEqualTo(PlanType.LITE);
+        assertThat(plan.getTokens().quota()).isEqualTo(PlanType.LITE.getQuota());
+        assertThat(plan.getTokens().remainingTokens()).isEqualTo(PlanType.LITE.getQuota());
+    }
+
+    @Test
+    @DisplayName("changePlanType 메서드 실패: PlanType이 같으면 예외 발생")
+    public void update_plan_success() throws Exception {
+        //given
+        Plan plan = Plan.createLite();
+
+        //when&then
+        assertThatThrownBy(() -> plan.changePlanType(PlanType.LITE))
+                .isInstanceOf(BaseException.class)
+                .hasMessage(PlanExceptionStatus.CANNOT_CHANGE_SAME_TYPE.getMessage());
+    }
+}
