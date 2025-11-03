3d8d759adee3fc91012650827575792ec0731166
jamm0316 <jamm0316@naver.com>
2025-11-03 16:33:25 +0900

    feat: User 엔티티 생성 메서드 및 검증 로직 추가
    1. User 생성 메서드 추가하여 객체 생성 책임 캡슐화한다.
    2. account, password, name 필드에 대한 유효성 검증 로직 추가한다.
    3. BaseException 활용하여 사용자 입력 오류 처리한다.
    
    Resolves #6

--- src/main/java/com/posicube/assignment/user/domain/entity/User.java
+++ src/main/java/com/posicube/assignment/user/domain/entity/User.java
@@ -1,9 +1,16 @@
 package com.posicube.assignment.user.domain.entity;
 
+import com.posicube.assignment.common.exception.BaseException;
 import com.posicube.assignment.plan.domain.entity.Plan;
+import com.posicube.assignment.user.exception.UserExceptionStatus;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id
     @GeneratedValue
@@ -22,4 +29,42 @@ public class User {
 
     @Column(nullable = false, length = 30)
     private String name;
+
+    private User(String account, String password, String name) {
+        validateUserInvariants(account, password, name);
+        plan = Plan.createLite();
+        this.account = account;
+        this.password = password;
+        this.name = name;
+    }
+
+    private void validateUserInvariants(String account, String password, String name) {
+        if (account == null || account.trim().isEmpty()) {
+            throw new BaseException(UserExceptionStatus.ACCOUNT_CANNOT_BE_NULL);
+        }
+
+        if (account.contains(" ")) {
+            throw new BaseException(UserExceptionStatus.ACCOUNT_CANNOT_CONTAIN_WHITESPACE);
+        }
+
+        if (password == null || password.trim().isEmpty()) {
+            throw new BaseException(UserExceptionStatus.PASSWORD_CANNOT_BE_NULL);
+        }
+
+        if (password.contains(" ")) {
+            throw new BaseException(UserExceptionStatus.PASSWORD_CANNOT_CONTAIN_WHITESPACE);
+        }
+
+        if (name == null || name.trim().isEmpty()) {
+            throw new BaseException(UserExceptionStatus.NAME_CANNOT_BE_NULL);
+        }
+
+        if (name.contains(" ")) {
+            throw new BaseException(UserExceptionStatus.NAME_CANNOT_CONTAIN_WHITESPACE);
+        }
+    }
+
+    public User create(String account, String password, String name) {
+        return new User(account, password, name);
+    }
 }
