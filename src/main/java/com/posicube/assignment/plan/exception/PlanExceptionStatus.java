9272fab4a591f79bc446e625a35e17acb84262ce
jamm0316 <jamm0316@naver.com>
2025-11-03 16:02:41 +0900

    feat: Plan 예외 상태 코드 추가
    1. PlanExceptionStatus에 새로운 예외 상태 코드 4개 추가한다.
    2. 예외 상태 코드별 메시지와 상세 내용을 정의한다.
    3. 기존 코드의 유효성을 유지하며 새로운 규칙 적용한다.
    
    Resolves #2

--- src/main/java/com/posicube/assignment/plan/exception/PlanExceptionStatus.java
+++ src/main/java/com/posicube/assignment/plan/exception/PlanExceptionStatus.java
@@ -8,7 +8,11 @@ public enum PlanExceptionStatus implements ResponseStatus {
     /**
      * Plan 오류
      */
-    INVALID_TOTAL_PRICE(false, "PLAN-1", "총 금액이 0이하일 수 없습니다."),
+    PLAN_TYPE_CANNOT_BE_NULL(false, "PLAN-1", "PLAN_TYPE은 null 일 수 없습니다."),
+    INVALID_TOTAL_PRICE(false, "PLAN-2", "총 금액이 0이하일 수 없습니다."),
+    CANNOT_CHANGE_SAME_TYPE(false, "PLAN-3", "같은 플랜으로 변경할 수 없습니다."),
+    TOKENS_CANNOT_BE_NULL(false, "PLAN-4", "토큰 정보는 필수입니다."),
+    INSUFFICIENT_TOKENS(false, "PLAN-5", "잔여 토큰이 부족합니다."),
     ;
 
     private final boolean isSuccess;
