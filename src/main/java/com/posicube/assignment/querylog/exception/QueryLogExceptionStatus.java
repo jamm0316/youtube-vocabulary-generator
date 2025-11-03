133599ad7762c582ce5d26d6afc0fca533cf4b55
jamm0316 <jamm0316@naver.com>
2025-11-04 05:38:22 +0900

    feat: QueryLogExceptionStatus Enum 클래스 추가
    1. QueryLog 관련 예외 상태 코드 Enum 클래스 추가한다.
    2. 질의 내용, 모델 타입, 사용자 정보 유효성 검증 예외 상태 정의한다.
    3. 오류 코드와 메시지를 통해 상세 오류 정보를 제공한다.
    
    Resolves #9

---
+++ src/main/java/com/posicube/assignment/querylog/exception/QueryLogExceptionStatus.java
@@ -0,0 +1,28 @@
+package com.posicube.assignment.querylog.exception;
+
+import com.posicube.assignment.common.baseResponse.ResponseStatus;
+import lombok.Getter;
+
+@Getter
+public enum QueryLogExceptionStatus implements ResponseStatus {
+    /**
+     * User 오류
+     */
+    QUERY_CANNOT_BE_NULL(false, "QUERY_LOG-1", "질의 내용은 null이거나 비어있을 수 없습니다."),
+    MODEL_TYPE_CANNOT_BE_NULL(false, "QUERY_LOG-2", "모델 타입은 null일 수 없습니다."),
+    INVALID_MODEL_TYPE(false, "QUERY_LOG-3", "유효하지 않은 모델 타입입니다."),
+    USER_CANNOT_BE_NULL(false, "QUERY_LOG-4", "사용자 정보는 null일 수 없습니다."),
+    QUERY_TOO_LONG(false, "QUERY_LOG-5", "쿼리는 800자 이내로 작성해야합니다."),
+    ;
+
+    private final boolean isSuccess;
+    private final String  code;
+    private final String message;
+
+    QueryLogExceptionStatus(boolean isSuccess, String code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
+
