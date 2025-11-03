fdad1d56dfd615622f1bdda225a2190ab9774db7
jamm0316 <jamm0316@naver.com>
2025-11-03 16:02:46 +0900

    feat: PlanType Enum 쿼터 관리 필드 추가
    1. PlanType Enum에 쿼터 관리 필드 quota 추가한다.
    2. LITE, PRO 플랜에 기본 쿼터 값 설정한다.
    3. 생성자를 통해 쿼터 값 초기화하도록 변경한다.
    
    Resolves #2

--- src/main/java/com/posicube/assignment/plan/domain/entity/PlanType.java
+++ src/main/java/com/posicube/assignment/plan/domain/entity/PlanType.java
@@ -1,5 +1,14 @@
 package com.posicube.assignment.plan.domain.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum PlanType {
-    LITE, PRO
+    LITE(10_000L), PRO(50_000L);
+
+    private final long quota;
+
+    PlanType(long quota) {
+        this.quota = quota;
+    }
 }
