1670e3e7fc36625b5a18f5ff67bcfbba0df9a855
jamm0316 <jamm0316@naver.com>
2025-11-04 11:18:22 +0900

    refactor: UserCreateRequest 유효성 검증 메시지 수정
    1. account와 password 필드의 최소 값 유효성 검증 메시지를 수정한다.
    2. 계정 및 비밀번호의 최소 길이를 1자로 명확히 안내한다.
    
    Resolves #14

--- src/main/java/com/posicube/assignment/users/presentation/dtos/UserCreateRequest.java
+++ src/main/java/com/posicube/assignment/users/presentation/dtos/UserCreateRequest.java
@@ -5,11 +5,11 @@ import jakarta.validation.constraints.Size;
 
 public record UserCreateRequest(
         @NotBlank(message = "account는 null 일 수 없습니다.")
-        @Size(min = 1, max = 100, message = "account는 3자 이상 100자 이하이어야 합니다.")
+        @Size(min = 1, max = 100, message = "account는 1자 이상 100자 이하이어야 합니다.")
         String account,
 
         @NotBlank(message = "password는 null 일 수 없습니다.")
-        @Size(min = 1, max = 100, message = "password는 8자 이상 100자 이하이어야 합니다.")
+        @Size(min = 1, max = 100, message = "password는 1자 이상 100자 이하이어야 합니다.")
         String password,
 
         @NotBlank(message = "name은 null 일 수 없습니다.")
