6c983b0641ba7f5f5c5322a1980c238b49428ce6
jamm0316 <jamm0316@naver.com>
2025-11-04 11:23:07 +0900

    feat: UserService 클래스 추가
    1. 사용자를 생성하는 createUser 메서드를 추가한다.
    2. PlanService 및 UserRepository를 활용하여 사용자와 플랜을 저장 처리한다.
    3. UserCreateRequest를 기반으로 Users 엔티티를 생성한다.
    
    Resolves #12

---
+++ src/main/java/com/posicube/assignment/users/application/UserService.java
@@ -0,0 +1,27 @@
+package com.posicube.assignment.users.application;
+
+import com.posicube.assignment.plan.application.PlanService;
+import com.posicube.assignment.plan.domain.entity.Plan;
+import com.posicube.assignment.users.domain.entity.Users;
+import com.posicube.assignment.users.domain.port.UserRepository;
+import com.posicube.assignment.users.presentation.dtos.UserCreateRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PlanService planService;
+
+    @Transactional
+    public Users createUser(UserCreateRequest request) {
+        Plan plan = planService.createPlan(request.plan());
+        Users users = Users.create(
+                request.account(), request.password(), request.name(), plan);
+        return userRepository.save(users);
+    }
+}
