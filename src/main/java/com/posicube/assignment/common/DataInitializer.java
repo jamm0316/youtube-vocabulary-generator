e109783eeb77c30c5233299911565895af504fd6
jamm0316 <jamm0316@naver.com>
2025-11-04 17:27:19 +0900

    refactor: DataInitializer 예외 처리 및 로깅 추가
    1. Plan 데이터 초기화 시 DataAccessException에 대한 예외 처리를 추가한다.
    2. 오류 발생 시 로그 메시지를 기록하도록 @Slf4j 어노테이션을 추가한다.
    3. 초기화 실패 시 RuntimeException을 발생시킨다.
    
    Resolves #17

--- src/main/java/com/posicube/assignment/common/DataInitializer.java
+++ src/main/java/com/posicube/assignment/common/DataInitializer.java
@@ -4,12 +4,15 @@ import com.posicube.assignment.plan.domain.entity.Plan;
 import com.posicube.assignment.plan.domain.port.PlanRepository;
 import com.posicube.assignment.plan.domain.vo.PlanType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements ApplicationRunner {
@@ -17,11 +20,17 @@ public class DataInitializer implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        if (planRepository.count() == 0) {
-            Plan litePlan = Plan.create(PlanType.LITE);
-            Plan proPlan = Plan.create(PlanType.PRO);
+        try {
+            if (planRepository.count() == 0) {
+                Plan litePlan = Plan.create(PlanType.LITE);
+                Plan proPlan = Plan.create(PlanType.PRO);
 
-            planRepository.saveAll(List.of(litePlan, proPlan));
+                planRepository.saveAll(List.of(litePlan, proPlan));
+            }
+        } catch (DataAccessException e) {
+            log.error("Plan 데이터 초기화에 실패했습니다.", e);
+            throw new RuntimeException("Plan 초기화 실패");
         }
+
     }
 }
