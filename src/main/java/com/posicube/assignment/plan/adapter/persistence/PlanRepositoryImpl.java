b1d3cea8bab86e5d76e3db59003fda0b1c0e3752
jamm0316 <jamm0316@naver.com>
2025-11-04 17:00:57 +0900

    refactor: PlanRepository 메서드 추가 및 구현 수정
    1. PlanRepository 인터페이스에 새로운 메서드를 추가한다.
    2. PlanRepositoryImpl 클래스에 findPlanByType, count, saveAll 메서드를 구현한다.
    3. SpringDataJpaPlanRepository에 findPlanByType 메서드를 추가한다.
    
    Resolves #17

--- src/main/java/com/posicube/assignment/plan/adapter/persistence/PlanRepositoryImpl.java
+++ src/main/java/com/posicube/assignment/plan/adapter/persistence/PlanRepositoryImpl.java
@@ -2,14 +2,29 @@ package com.posicube.assignment.plan.adapter.persistence;
 
 import com.posicube.assignment.plan.domain.entity.Plan;
 import com.posicube.assignment.plan.domain.port.PlanRepository;
+import com.posicube.assignment.plan.domain.vo.PlanType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class PlanRepositoryImpl implements PlanRepository {
     private final SpringDataJpaPlanRepository jpa;
 
-    @Override
     public Plan save(Plan plan) { return jpa.save(plan); }
+
+    public Optional<Plan> findPlanByType(PlanType type) {
+        return jpa.findPlanByType(type);
+    }
+
+    public long count() {
+        return jpa.count();
+    }
+
+    public List<Plan> saveAll(List<Plan> plans) {
+        return jpa.saveAll(plans);
+    }
 }
--- src/main/java/com/posicube/assignment/plan/adapter/persistence/SpringDataJpaPlanRepository.java
+++ src/main/java/com/posicube/assignment/plan/adapter/persistence/SpringDataJpaPlanRepository.java
@@ -4,6 +4,8 @@ import com.posicube.assignment.plan.domain.entity.Plan;
 import com.posicube.assignment.plan.domain.vo.PlanType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SpringDataJpaPlanRepository extends JpaRepository<Plan, PlanType> {
+import java.util.Optional;
 
+public interface SpringDataJpaPlanRepository extends JpaRepository<Plan, PlanType> {
+    Optional<Plan> findPlanByType(PlanType type);
 }
