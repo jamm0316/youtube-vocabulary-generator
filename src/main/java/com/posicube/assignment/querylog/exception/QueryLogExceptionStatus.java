4f479177d09b60bf88ce4c4d784faac547197791
jamm0316 <jamm0316@naver.com>
2025-11-05 06:04:12 +0900

    feat: QueryLog 예외 상태코드 및 메시지 추가/수정
    1. MODEL_TYPE_CANNOT_BE_NULL 메시지를 null 및 비어있음 검증으로 수정한다.
    2. QUERY_TOO_LONG 메시지에 최소 글자 수 검증 조건을 추가한다.
    3. TOO_MANY_REQUESTS 예외 상태코드 및 메시지를 신규 추가한다.
    
    Resolves #19

--- src/main/java/com/posicube/assignment/querylog/exception/QueryLogExceptionStatus.java
+++ src/main/java/com/posicube/assignment/querylog/exception/QueryLogExceptionStatus.java
@@ -9,10 +9,11 @@ public enum QueryLogExceptionStatus implements ResponseStatus {
      * QueryLog 오류
      */
     QUERY_CANNOT_BE_NULL(false, "QUERY_LOG-1", "질의 내용은 null이거나 비어있을 수 없습니다."),
-    MODEL_TYPE_CANNOT_BE_NULL(false, "QUERY_LOG-2", "모델 타입은 null일 수 없습니다."),
+    MODEL_TYPE_CANNOT_BE_NULL(false, "QUERY_LOG-2", "모델 타입은 null이거나 비어있을 수 없습니다."),
     INVALID_MODEL_TYPE(false, "QUERY_LOG-3", "유효하지 않은 모델 타입입니다."),
     USER_CANNOT_BE_NULL(false, "QUERY_LOG-4", "사용자 정보는 null일 수 없습니다."),
-    QUERY_TOO_LONG(false, "QUERY_LOG-5", "쿼리는 800자 이내로 작성해야합니다."),
+    QUERY_TOO_LONG(false, "QUERY_LOG-5", "쿼리는 1자 이상 800자 이내로 작성해야합니다."),
+    TOO_MANY_REQUESTS(false, "QUERY_LOG-6", "1분 이내 요청 30개를 초과할 수 없습니다."),
     ;
 
     private final boolean isSuccess;
