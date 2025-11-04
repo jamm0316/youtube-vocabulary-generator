229b7f1f7741595b5b30048cd6f8c74e3ca0ae8d
jamm0316 <jamm0316@naver.com>
2025-11-04 11:12:50 +0900

    refactor: UserCreateRequest 유효성 검증 수정 및 PlanType 삭제
    1. account와 password의 최소 문자열 길이를 1자로 수정한다.
    2. name 필드 유효성 검증 메시지를 수정한다.
    3. PlanType 필드를 제거하고 plan 문자열 필드를 추가한다.
    4. 정적 팩토리 메서드 create를 삭제한다.
    
    Resolves #14

--- src/main/java/com/posicube/assignment/users/presentation/dtos/UserCreateRequest.java
+++ src/main/java/com/posicube/assignment/users/presentation/dtos/UserCreateRequest.java
@@ -1,27 +1,23 @@
 package com.posicube.assignment.users.presentation.dtos;
 
-import com.posicube.assignment.plan.domain.entity.PlanType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserCreateRequest(
         @NotBlank(message = "account는 null 일 수 없습니다.")
-        @Size(min = 3, max = 100, message = "account는 3자 이상 100자 이하이어야 합니다.")
+        @Size(min = 1, max = 100, message = "account는 3자 이상 100자 이하이어야 합니다.")
         String account,
 
         @NotBlank(message = "password는 null 일 수 없습니다.")
-        @Size(min = 8, max = 100, message = "password는 8자 이상 100자 이하이어야 합니다.")
+        @Size(min = 1, max = 100, message = "password는 8자 이상 100자 이하이어야 합니다.")
         String password,
 
         @NotBlank(message = "name은 null 일 수 없습니다.")
-        @Size(min = 8, max = 30, message = "name은 1자 이상 30 이하이어야 합니다.")
+        @Size(min = 1, max = 30, message = "name은 1자 이상 30 이하이어야 합니다.")
         String name,
 
-        PlanType plan
-
+        @NotBlank(message = "planType은 null 일 수 없습니다.")
+        String plan
 ) {
 
-    public static UserCreateRequest create(String account, String password, String name, PlanType plan) {
-        return new UserCreateRequest(account, password, name, plan);
-    }
 }
