4a491395b8b51407c8f0b54531d1b046622aa13e
jamm0316 <jamm0316@naver.com>
2025-11-04 11:23:18 +0900

    feat: UserController 클래스 추가
    1. 사용자를 생성하는 create 메서드를 추가한다.
    2. UserService와 연계하여 사용자 생성 비즈니스 로직을 호출한다.
    3. UserCreateRequest를 전달받아 UserInfoResponse로 반환한다.
    
    Resolves #12

---
+++ src/main/java/com/posicube/assignment/users/presentation/UserController.java
@@ -0,0 +1,25 @@
+package com.posicube.assignment.users.presentation;
+
+import com.posicube.assignment.users.application.UserService;
+import com.posicube.assignment.users.domain.entity.Users;
+import com.posicube.assignment.users.presentation.dtos.UserCreateRequest;
+import com.posicube.assignment.users.presentation.dtos.UserInfoResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("")
+    public UserInfoResponse create(@Valid @RequestBody UserCreateRequest request) {
+        Users user = userService.createUser(request);
+        return UserInfoResponse.from(user);
+    }
+}
