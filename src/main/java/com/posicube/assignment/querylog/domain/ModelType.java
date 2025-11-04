9fb38ae92aa14dff1ac04c0ac1495e663d39a686
jamm0316 <jamm0316@naver.com>
2025-11-05 06:03:57 +0900

    feat: ModelType 열거형 개선 및 성능 최적화
    1. from 메서드에서 Stream 처리 로직을 Map 기반 조회로 변경한다.
    2. stringToEnum 정적 맵을 추가하여 문자열과 Enum 간 매핑 성능을 개선한다.
    3. Optional을 활용한 from 메서드 입력값 검증 및 예외 처리를 강화한다.
    
    Resolves #19

--- src/main/java/com/posicube/assignment/querylog/domain/ModelType.java
+++ src/main/java/com/posicube/assignment/querylog/domain/ModelType.java
@@ -5,7 +5,11 @@ import com.posicube.assignment.querylog.exception.QueryLogExceptionStatus;
 import lombok.Getter;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Getter
 public enum ModelType {
@@ -13,16 +17,21 @@ public enum ModelType {
 
     private final String name;
     private final BigDecimal pricePer1KToken;
+    private static final Map<String, ModelType> stringToEnum
+            = Stream.of(values())
+            .collect(Collectors.toMap(Enum::name, Function.identity()));
 
     ModelType(String name, String pricePer1KToken) {
+
         this.name = name;
         this.pricePer1KToken = new BigDecimal(pricePer1KToken);
     }
 
+
     public static ModelType from(String name) {
-        return Arrays.stream(values())
-                .filter(modelType -> modelType.name.equalsIgnoreCase(name))
-                .findFirst()
+        return Optional.ofNullable(name)
+                .map(String::toLowerCase)
+                .map(stringToEnum::get)
                 .orElseThrow(() -> new BaseException(QueryLogExceptionStatus.INVALID_MODEL_TYPE));
     }
 }
