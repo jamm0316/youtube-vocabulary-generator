df287f5eee49ecb068f006b425d151e0cf5901ef
jamm0316 <jamm0316@naver.com>
2025-11-05 06:02:25 +0900

    feat: UserRepository에 findUserById 메서드 추가
    1. SpringDataJpaUserRepository에 findUsersById 메서드를 추가한다.
    2. UserRepository 인터페이스에 findUserById 메서드를 정의한다.
    3. UserRepositoryImpl에 findUserById 메서드 구현을 추가한다.
    
    Resolves #19

--- src/main/java/com/posicube/assignment/users/adapter/persistence/SpringDataJpaUserRepository.java
+++ src/main/java/com/posicube/assignment/users/adapter/persistence/SpringDataJpaUserRepository.java
@@ -3,6 +3,9 @@ package com.posicube.assignment.users.adapter.persistence;
 import com.posicube.assignment.users.domain.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SpringDataJpaUserRepository extends JpaRepository<Users, Long> {
 
+    Optional<Users> findUsersById(Long id);
 }
