c00aa28f8698b8f55742a0b334a30cb3689e3310
jamm0316 <jamm0316@naver.com>
2025-11-05 12:14:33 +0900

    feat: QueryResponse 정적 팩토리 메서드 추가
    1. QueryResponse에 from 정적 팩토리 메서드를 추가한다.
    2. 입력값 answer를 기반으로 객체 생성 역할을 담당한다.
    
    Resolves #19

--- src/main/java/com/posicube/assignment/querylog/presentation/dtos/QueryResponse.java
+++ src/main/java/com/posicube/assignment/querylog/presentation/dtos/QueryResponse.java
@@ -1,6 +1,9 @@
 package com.posicube.assignment.querylog.presentation.dtos;
 
 public record QueryResponse (
-
+        String answer
 ) {
+    public static QueryResponse from(String answer) {
+        return new QueryResponse(answer);
+    }
 }
