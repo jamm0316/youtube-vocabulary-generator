b4c03a8c04663e0ff8606e9dcd79150806c1df43
jamm0316 <jamm0316@naver.com>
2025-11-07 07:12:36 +0900

    test: 사용자 토큰 초기화 스케줄러 테스트 추가
    1. `TokenResetSchedulerTest` 클래스를 생성하여 사용자 토큰 초기화 스케줄러를 검증한다.
    2. 모든 사용자의 토큰 사용량 초기화 로직을 테스트한다.
    3. 사용자 플랜에 따른 토큰 초기화 상태를 검증한다.
    
    Resolves #25

---
+++ src/test/java/com/posicube/assignment/domain/users/TokenResetSchedulerTest.java
@@ -0,0 +1,52 @@
+package com.posicube.assignment.domain.users;
+
+import com.posicube.assignment.plan.domain.model.PlanType;
+import com.posicube.assignment.users.application.commandquery.UserCreateRequest;
+import com.posicube.assignment.users.application.commandquery.UserInfoResponse;
+import com.posicube.assignment.users.application.service.TokenResetScheduler;
+import com.posicube.assignment.users.application.service.UsersService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.PATH;
+
+@SpringBootTest
+@Transactional
+public class TokenResetSchedulerTest {
+    @Autowired private TokenResetScheduler tokenResetScheduler;
+    @Autowired private UsersService usersService;
+
+    @BeforeEach
+    void setUp() {
+        usersService.createUser(new UserCreateRequest("test1", "test1234", "evan", "LITE"));
+        usersService.createUser(new UserCreateRequest("test2", "test5678", "sofia", "PRO"));
+    }
+
+    @Test
+    @DisplayName("모든 사용자의 토큰 사용량을 성공적으로 초기화 한다.")
+    public void resetAllUsersTokens_shouldReset() throws Exception {
+        //given
+        tokenResetScheduler.resetAllUserTokens();
+
+        //when
+        List<UserInfoResponse> allUsersInfo = usersService.findAllUsersInfo();
+        assertThat(allUsersInfo.size()).isEqualTo(2);
+
+        //then
+        allUsersInfo.forEach(user -> {
+            if (user.plan().equals(PlanType.LITE)) {
+                assertThat(user.tokens().getRemainingTokens()).isEqualTo(PlanType.LITE.getQuota());
+            } else {
+                assertThat(user.tokens().getRemainingTokens()).isEqualTo(PlanType.PRO.getQuota());
+            }
+            assertThat(user.tokens().getUsedTokens()).isEqualTo(0);
+        });
+    }
+}
