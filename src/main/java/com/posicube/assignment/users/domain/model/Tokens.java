de5503cde2c10003f13a54bd39d48868aa3fe8cc
jamm0316 <jamm0316@naver.com>
2025-11-07 06:11:27 +0900

    refactor: Users 및 Tokens 클래스 메서드 추가
    1. Users 클래스에 resetToken 메서드를 추가한다.
    2. Tokens 클래스에 reset 메서드를 추가해 토큰 초기화 로직 구현한다.
    3. 코드 구조를 개선하여 처리 로직을 간결화한다.
    
    Resolves #25

--- src/main/java/com/posicube/assignment/users/domain/model/Tokens.java
+++ src/main/java/com/posicube/assignment/users/domain/model/Tokens.java
@@ -23,4 +23,8 @@ public class Tokens {
         }
         return new Tokens(this.quota, newUsedTokens, newRemainingTokens);
     }
+
+    public Tokens reset() {
+        return new Tokens(this.quota, 0L, this.quota);
+    }
 }
