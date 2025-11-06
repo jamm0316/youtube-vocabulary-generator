371d9dfaf876e84a20d74917a793855fb86caed7
jamm0316 <jamm0316@naver.com>
2025-11-06 17:11:11 +0900

    refactor: QueryLogRepository 사용자 기반 조회 메서드 추가
    1. QueryLogRepository에 findAllByUserId 메서드 추가한다.
    2. SpringDataJpaQueryLogRepository와 구현체에 사용자 기반 조회 로직 추가한다.
    3. QueryLogMapper를 활용해 데이터 객체를 변환한다.
    
    Resolves #21

--- src/main/java/com/posicube/assignment/querylog/adapter/out/persistence/QueryLogRepositoryImpl.java
+++ src/main/java/com/posicube/assignment/querylog/adapter/out/persistence/QueryLogRepositoryImpl.java
@@ -9,6 +9,9 @@ import com.posicube.assignment.users.exception.UserExceptionStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Repository
 @RequiredArgsConstructor
 public class QueryLogRepositoryImpl implements QueryLogRepository {
@@ -25,5 +28,17 @@ public class QueryLogRepositoryImpl implements QueryLogRepository {
 
         QueryLogJpaEntity savedEntity = jpaQueryLogRepository.save(entityToSave);
         return queryLogMapper.toDomain(savedEntity);
+    }
+
+    @Override
+    public List<QueryLog> findAllByUserId(Long userId) {
+        UsersJpaEntity userEntity = jpaUserRepository.findUsersById(userId)
+                .orElseThrow(() -> new BaseException(UserExceptionStatus.USER_NOT_FOUND));
+
+        List<QueryLogJpaEntity> allByUser = jpaQueryLogRepository.findAllByUser(userEntity);
+
+        return allByUser.stream()
+                .map(queryLogMapper::toDomain)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/posicube/assignment/querylog/port/out/QueryLogRepository.java
+++ src/main/java/com/posicube/assignment/querylog/port/out/QueryLogRepository.java
@@ -2,6 +2,9 @@ package com.posicube.assignment.querylog.port.out;
 
 import com.posicube.assignment.querylog.domain.model.QueryLog;
 
+import java.util.List;
+
 public interface QueryLogRepository {
     QueryLog save(QueryLog queryLog);
+    List<QueryLog> findAllByUserId(Long userId);
 }
