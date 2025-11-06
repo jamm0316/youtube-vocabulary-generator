a01fa0ec9b30d1b62de4bd5333f3cef86c195b10
jamm0316 <jamm0316@naver.com>
2025-11-07 07:12:53 +0900

    refactor: 불필요한 import 제거
    1. TokenResetSchedulerTest 클래스에서 사용되지 않는 PATH 관련 import를 제거한다.
    2. 코드 가독성을 향상하고 유지보수를 용이하게 한다.
    
    Resolves #25

--- src/test/java/com/posicube/assignment/domain/users/TokenResetSchedulerTest.java
+++ src/test/java/com/posicube/assignment/domain/users/TokenResetSchedulerTest.java
@@ -15,7 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.PATH;
 
 @SpringBootTest
 @Transactional
