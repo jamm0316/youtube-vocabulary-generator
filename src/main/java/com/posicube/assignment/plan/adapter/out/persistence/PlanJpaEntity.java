ca74fa93093ebae8a991753047e284f9b8ba9d59
jamm0316 <jamm0316@naver.com>
2025-11-06 10:13:15 +0900

    refactor: PlanJpaEntity에 @Table 어노테이션 추가 및 최적화
    1. PlanJpaEntity 클래스에 @Table(name = "plan") 어노테이션을 추가한다.
    2. import 구문 정리 및 최적화를 통해 코드 가독성을 개선한다.
    
    Resolves #22

--- src/main/java/com/posicube/assignment/plan/adapter/out/persistence/PlanJpaEntity.java
+++ src/main/java/com/posicube/assignment/plan/adapter/out/persistence/PlanJpaEntity.java
@@ -1,15 +1,13 @@
 package com.posicube.assignment.plan.adapter.out.persistence;
 
 import com.posicube.assignment.plan.domain.model.PlanType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "plan")
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
