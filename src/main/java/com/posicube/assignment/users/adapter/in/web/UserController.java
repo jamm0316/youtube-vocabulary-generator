2b3e3d25eddbaf8a784a99f02bf1c9280d087293
jamm0316 <jamm0316@naver.com>
2025-11-06 17:12:49 +0900

    feat: 사용자 사용량 조회 API 추가
    1. `/users/usage` 엔드포인트 및 관련 메서드 추가한다.
    2. 헤더에서 `X-User-Id`를 받아 사용자 사용량 데이터를 조회한다.
    3. `UsageResponse` 객체로 응답 데이터를 반환한다.
    
    Resolves #21

--- src/main/java/com/posicube/assignment/users/adapter/in/web/UserController.java
+++ src/main/java/com/posicube/assignment/users/adapter/in/web/UserController.java
@@ -1,6 +1,7 @@
 package com.posicube.assignment.users.adapter.in.web;
 
 import com.posicube.assignment.common.baseResponse.BaseResponse;
+import com.posicube.assignment.users.application.commandquery.UsageResponse;
 import com.posicube.assignment.users.application.commandquery.UserCreateRequest;
 import com.posicube.assignment.users.application.commandquery.UserInfoResponse;
 import com.posicube.assignment.users.application.service.UsersService;
@@ -10,19 +11,23 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UsersService usersService;
 
-    @PostMapping("")
+    @PostMapping("/users")
     public BaseResponse<UserInfoResponse> create(@Valid @RequestBody UserCreateRequest request) {
         Users user = usersService.createUser(request);
         return new BaseResponse<>(UserInfoResponse.from(user));
+    }
+
+    @PostMapping("/usage")
+    public BaseResponse<UsageResponse> getUsage(@RequestHeader("X-User-Id") Long userId) {
+        return new BaseResponse<>(usersService.getUserUsage(userId));
     }
 }
