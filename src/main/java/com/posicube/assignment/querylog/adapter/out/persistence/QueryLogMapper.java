f3bf8c86fee02333d7f7e4c72f52dd7e061e551b
jamm0316 <jamm0316@naver.com>
2025-11-06 09:10:58 +0900

    refactor: QueryLogJpaEntity 및 QueryLogMapper 추가
    1. QueryLogJpaEntity 클래스를 추가하여 JPA 엔티티를 정의한다.
    2. QueryLogMapper 클래스를 추가하여 도메인 모델과 JPA 엔티티 간 변환을 구현한다.
    3. 기존 QueryLog 도메인 클래스에서 유효성 검증 및 생성자 로직을 제거한다.
    4. 관련된 import 경로를 수정하여 변경 사항을 반영한다.
    
    Resolves #22

---
+++ src/main/java/com/posicube/assignment/querylog/adapter/out/persistence/QueryLogMapper.java
@@ -0,0 +1,37 @@
+package com.posicube.assignment.querylog.adapter.out.persistence;
+
+import com.posicube.assignment.querylog.domain.model.QueryLog;
+import com.posicube.assignment.users.adapter.out.persistence.UsersJpaEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryLogMapper {
+
+    //JPA Entity -> Domain Model
+    public QueryLog toDomain(QueryLogJpaEntity entity) {
+        return QueryLog.builder()
+                .id(entity.getId())
+                .userId(entity.getUser().getId())
+                .type(entity.getType())
+                .content(entity.getContent())
+                .answer(entity.getAnswer())
+                .usedTokens(entity.getUsedTokens())
+                .cost(entity.getCost())
+                .createAt(entity.getCreateAt())
+                .build();
+    }
+
+    //Domain Model -> JPA Entity
+    public QueryLogJpaEntity toEntity(QueryLog domain, UsersJpaEntity usersJpaEntityEntity) {
+        return QueryLogJpaEntity.builder()
+                .id(domain.getId())
+                .user(usersJpaEntityEntity)
+                .type(domain.getType())
+                .content(domain.getContent())
+                .answer(domain.getAnswer())
+                .usedTokens(domain.getUsedTokens())
+                .cost(domain.getCost())
+                .createAt(domain.getCreateAt())
+                .build();
+    }
+}
