037ec8fd58748600d28a419aad2982e8b50c3b2c
jamm0316 <jamm0316@naver.com>
2025-11-07 10:32:29 +0900

    refactor: GlobalExceptionHandler 리팩토링 및 SuccessResponseAdvice 추가
    1. GlobalExceptionHandler에 ResponseEntity를 반환하도록 수정한다.
    2. SuccessResponseAdvice 추가로 성공 응답 통일성을 제공한다.
    3. 예외 처리 시 HTTP 상태 코드와 메시지를 명확히 전달한다.
    
    Resolves #29

--- src/main/java/com/posicube/assignment/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/posicube/assignment/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.posicube.assignment.common.exception;
 import com.posicube.assignment.common.baseResponse.BaseResponse;
 import com.posicube.assignment.common.baseResponse.BaseResponseStatus;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,25 +15,28 @@ public class GlobalExceptionHandler {
      * BaseException (커스텀 예외) 발생 시 처리하는 핸들러
      */
     @ExceptionHandler(BaseException.class)
-    public BaseResponse<Object> handleBaseException(BaseException e, HttpServletRequest request) {
-        return new BaseResponse<>(e.getStatus());
+    public ResponseEntity<BaseResponse<Object>> handleBaseException(BaseException e, HttpServletRequest request) {
+        BaseResponse<Object> response = new BaseResponse<>(e.getStatus());
+        return new ResponseEntity<>(response, e.getStatus().getHttpStatus());
     }
 
     /**
      * 그 외 예상하지 못한 Exception 발생 시 처리하는 핸들러
      */
     @ExceptionHandler(Exception.class)
-    public BaseResponse<Object> handleException(Exception e) {
+    public ResponseEntity<BaseResponse<Object>> handleException(Exception e) {
         e.printStackTrace();
-        return new BaseResponse<>(BaseResponseStatus.INTERNAL_SERVER_ERROR);
+        BaseResponse<Object> response = new BaseResponse<>(BaseResponseStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, BaseResponseStatus.INTERNAL_SERVER_ERROR.getHttpStatus());
     }
 
     /**
      * DTO @Valid 실패 시 발생하는 Validation 에러 처리 핸들러
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public BaseResponse<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    public ResponseEntity<BaseResponse<Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         String errorMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-        return new BaseResponse<>(BaseResponseStatus.VALIDATION_ERROR, errorMessage);
+        BaseResponse<Object> response = new BaseResponse<>(BaseResponseStatus.VALIDATION_ERROR, errorMessage);
+        return new ResponseEntity<>(response, BaseResponseStatus.VALIDATION_ERROR.getHttpStatus());
     }
 }
