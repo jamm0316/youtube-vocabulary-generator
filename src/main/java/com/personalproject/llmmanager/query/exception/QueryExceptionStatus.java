e8e3acff19c74554a803b2cbc5b6c53a9016452d
jamm0316 <jamm0316@naver.com>
2025-11-29 04:59:28 +0900

    refactor: QueryExceptionStatus 상태 값 추가
    1. USER_CANNOT_BE_NULL 상태 값 추가한다.
    2. URL_CANNOT_BE_NULL_OR_BLANK 상태 값 추가한다.
    3. MODEL_TYPE_CANNOT_BE_NULL 상태 값 추가한다.
    4. TOKEN_CALCULATION_IS_ONLY_POSSIBLE_IN_PENDING 상태 값 추가한다.
    
    Resolves #49

--- src/main/java/com/personalproject/llmmanager/query/exception/QueryExceptionStatus.java
+++ src/main/java/com/personalproject/llmmanager/query/exception/QueryExceptionStatus.java
@@ -11,6 +11,10 @@ public enum QueryExceptionStatus implements ResponseStatus {
      */
     TOKEN_CALCULATION_IS_REQUIRED(false, "QUERY-1", "토큰 계산이 먼저 필요합니다.", HttpStatus.BAD_REQUEST),
     QUERY_IS_NOT_COMPLETE(false, "QUERY-2", "쿼리가 완료되지 않았습니다.", HttpStatus.BAD_REQUEST),
+    USER_CANNOT_BE_NULL(false, "QUERY-3", "user가 null이면 안됩니다.", HttpStatus.BAD_REQUEST),
+    URL_CANNOT_BE_NULL_OR_BLANK(false, "QUERY-4", "url이 null이면 안됩니다.", HttpStatus.BAD_REQUEST),
+    MODEL_TYPE_CANNOT_BE_NULL(false, "QUERY-4", "url이 null이면 안됩니다.", HttpStatus.BAD_REQUEST),
+    TOKEN_CALCULATION_IS_ONLY_POSSIBLE_IN_PENDING(false, "QUERY-4", "url이 null이면 안됩니다.", HttpStatus.BAD_REQUEST),
     ;
 
     private final boolean isSuccess;
