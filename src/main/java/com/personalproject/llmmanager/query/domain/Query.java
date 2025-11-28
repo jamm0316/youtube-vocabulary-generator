971dfdce37e3d2c5162d1f0f6f8221ec18473592
jamm0316 <jamm0316@naver.com>
2025-11-29 04:59:22 +0900

    refactor: Query 도메인 생성자 및 인variants 검증 추가
    1. 불필요한 AllArgsConstructor 제거와 명시적 생성자 추가한다.
    2. Query 생성 시 필수 값 검증 로직(validationQueryInvariants) 추가한다.
    3. 객체 생성 과정에서 데이터 유효성을 강화한다.
    
    Resolves #49

--- src/main/java/com/personalproject/llmmanager/query/domain/Query.java
+++ src/main/java/com/personalproject/llmmanager/query/domain/Query.java
@@ -6,14 +6,14 @@ import com.personalproject.llmmanager.querylog.domain.model.ModelType;
 import com.personalproject.llmmanager.querylog.domain.model.QueryLog;
 import com.personalproject.llmmanager.querylog.domain.policy.TokenCalculator;
 import com.personalproject.llmmanager.users.domain.model.Users;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.Objects;
 
 /**
  * Query 애그리게잇 루트
  * Users와 QueryLog의 협력을 조율하는 중심 도메인
  */
-@AllArgsConstructor
 @Getter
 public class Query {
     private final Users user;
@@ -23,12 +23,39 @@ public class Query {
     private String answer;
     private QueryStatus status;
 
+    private Query(Users user, String url, ModelType modelType, Long usedTokens, String answer, QueryStatus status) {
+        this.user = user;
+        this.url = url;
+        this.modelType = modelType;
+        this.usedTokens = usedTokens;
+        this.answer = answer;
+        this.status = status;
+    }
+
     public static Query start(Users user, String url, ModelType modelType) {
+        validationQueryInvariants(user, url, modelType);
         user.validateQueryPermission();
         return new Query(user, url, modelType, null, null, QueryStatus.PENDING);
     }
 
+    private static void validationQueryInvariants(Users user, String url, ModelType modelType) {
+        if (Objects.isNull(user)) {
+            throw new BaseException(QueryExceptionStatus.USER_CANNOT_BE_NULL);
+        }
+
+        if (Objects.isNull(url) || url.isBlank()) {
+            throw new BaseException(QueryExceptionStatus.URL_CANNOT_BE_NULL_OR_BLANK);
+        }
+
+        if (Objects.isNull(modelType)) {
+            throw new BaseException(QueryExceptionStatus.MODEL_TYPE_CANNOT_BE_NULL);
+        }
+    }
+
     public Query calculateTokens(TokenCalculator calculator) {
+        if (status != QueryStatus.PENDING) {
+            throw new BaseException(QueryExceptionStatus.TOKEN_CALCULATION_IS_ONLY_POSSIBLE_IN_PENDING);
+        }
         this.usedTokens = calculator.calculateTokensFromPrompt(this.url);
         return this;
     }
